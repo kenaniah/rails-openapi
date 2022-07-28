7ef73e148671dc6ec5cb87e6d61cdb63aed29c8d
Kenaniah Cerny <kenaniah@gmail.com>
2022-07-27 17:10:28 -0700

    gem init

---
+++ lib/rails/openapi.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "openapi/version"
+
+module Rails
+  module Openapi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
